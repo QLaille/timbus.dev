964b8a3cf4aa45b1cd99d2d6f129715b2221ea12
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-29 13:30:02 +0000

    Deploy QLaille/timbus.dev to github.com/QLaille/timbus.dev.git:gh-pages

--- _nuxt/pages/about.29b2cbe.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{159:function(n,t,e){"use strict";e.r(t);var c={},o=e(26),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  About \n")])}),[],!1,null,"24e1bb29",null);t.default=component.exports}}]);
